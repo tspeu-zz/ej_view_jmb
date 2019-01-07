435bcb30078ebeccdd3576bc1963760d863926e8
tspeu <tenshotseuropa@gmail.com>
2019-01-07 16:10:17 +0000

    add check workers

--- src/app/matching/matching.component.ts
+++ src/app/matching/matching.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, FormArray, ValidatorFn } from '@angular/forms';
 import {MatSnackBar} from '@angular/material';
 import {HttpService} from '../services/http.service';
 
@@ -26,47 +26,6 @@ export class MatchingComponent implements OnInit {
     workers: any;
     shifts: any;
 
-
-  workersTest = [
-    {
-      'id': 1,
-      'availability': ['Monday', 'Wednesday'],
-      'payrate': 7.50
-    },
-    {
-      'id': 2,
-      'availability': ['Monday', 'Tuesday', 'Thursday'],
-      'payrate': 9.00
-    },
-    {
-      'id': 3,
-      'availability': ['Monday', 'Friday'],
-      'payrate': 18.00
-    },
-    {
-      'id': 4,
-      'availability': ['Monday', 'Tuesday', 'Friday'],
-      'payrate': 12.25
-    }
-  ];
-  shiftsTest = [
-    {
-      'id': 1,
-      'day': ['Monday']
-    },
-    {
-      'id': 2,
-      'day': ['Tuesday']
-    },
-    {
-      'id': 3,
-      'day': ['Wednesday']
-    },
-    {
-      'id': 4,
-      'day': ['Thursday']
-    }
-  ];
   data = {
     'workers': [
       {
@@ -110,19 +69,73 @@ export class MatchingComponent implements OnInit {
     ]
   };
 
-  constructor(public snackBar: MatSnackBar,
-              private httpService: HttpService) { }
+  // orders = [
+  //   { id: 100, name: 'order 1' },
+  //   { id: 200, name: 'order 2' },
+  //   { id: 300, name: 'order 3' },
+  //   { id: 400, name: 'order 4' }
+  // ];
+  selectedWorkers: any = [];
+  selectedShifts: any = [];
 
-  ngOnInit() {
+  // SEND DATA TO API
+  outData = { 'workers': [], 'shifts': [] };
 
+  matchingForm: FormGroup;
+  form: FormGroup;
 
-    // this.postData(this.data);
+  constructor(public snackBar: MatSnackBar, private httpService: HttpService,
+              private formBuilder: FormBuilder) {
+
+    const controls = this.data.workers.map(c => new FormControl(false));
+    controls[0].setValue(true);
+    // console.log('controls', controls);
+
+    this.form = this.formBuilder.group({
+      orders: new FormArray(controls, this.minSelectedCheckboxes(1))
+    });
+    // console.log('this.form', this.form);
   }
 
-  sendDataPost() {
+  ngOnInit() {/* this.postData(this.data);*/}
 
+  onSubmit() {
+    const selectedOrderIds = this.form.value.orders
+      .map((v, i) => v ? this.data.workers[i].id : null)
+      .filter(v => v !== null);
+
+    // console.log('selectedOrderIds->', selectedOrderIds);
+    for (let i = 0; i < this.data.workers.length; i++) {
+        // console.log('workers', this.data.workers[i]);
+        for ( let j = 0; j < selectedOrderIds.length; j++ ) {
+            // console.log('select', selectedOrderIds[j]);
+            if (this.data.workers[i].id === selectedOrderIds[j]) {
+              this.outData.workers.push(this.data.workers[i]);
+              console.log('DEBE HACER PUSH A LOS QUE SE ENVIA->', this.data.workers[i] );
+            }
+        }
+    }
+    console.log('this.outData.workers', this.outData);
+    this.sendDataPost(this.outData);
+  }
+
+  minSelectedCheckboxes(min = 1) {
+    const validator: ValidatorFn = (formArray: FormArray) => {
+      const totalSelected = formArray.controls
+        .map(control => control.value)
+        .reduce((prev, next) => next ? prev + next : prev, 0);
+
+      return totalSelected >= min ? null : { required: true };
+    };
+    console.log('validator--->', validator);
+    return validator;
+  }
+
+  sendDataPost(data) {
+    // this.selectedWorkers = this.matchingGroup.value;
+    console.log('--', this.selectedWorkers);
   return this.httpService
-          .sendPost(this.data, this._URL)
+          .sendPost(data, this._URL)
           .subscribe(resp => {
               this._RESPONSE = resp;
               this.matching = this._RESPONSE.res;
@@ -135,6 +148,7 @@ export class MatchingComponent implements OnInit {
               this.show = true;
             }, error => { console.log(error);
           });
+    console.log(data);
   }
 
   addWorkers(data) {
@@ -175,3 +189,56 @@ export class MatchingComponent implements OnInit {
   }
 
 }
+function minSelectedCheckboxes(min = 1) {
+  const validator: ValidatorFn = (formArray: FormArray) => {
+    const totalSelected = formArray.controls
+      .map(control => control.value)
+      .reduce((prev, next) => next ? prev + next : prev, 0);
+
+    return totalSelected >= min ? null : { required: true };
+    console.log('validator--->', validator);
+
+  };
+  return validator;
+}
+/*
+  workersTest = [
+    {
+      'id': 1,
+      'availability': ['Monday', 'Wednesday'],
+      'payrate': 7.50
+    },
+    {
+      'id': 2,
+      'availability': ['Monday', 'Tuesday', 'Thursday'],
+      'payrate': 9.00
+    },
+    {
+      'id': 3,
+      'availability': ['Monday', 'Friday'],
+      'payrate': 18.00
+    },
+    {
+      'id': 4,
+      'availability': ['Monday', 'Tuesday', 'Friday'],
+      'payrate': 12.25
+    }
+  ];
+  shiftsTest = [
+    {
+      'id': 1,
+      'day': ['Monday']
+    },
+    {
+      'id': 2,
+      'day': ['Tuesday']
+    },
+    {
+      'id': 3,
+      'day': ['Wednesday']
+    },
+    {
+      'id': 4,
+      'day': ['Thursday']
+    }
+  ]; */
