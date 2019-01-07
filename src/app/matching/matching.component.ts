738bd7556c2203cf65e131e52414689a3285eea7
tspeu <tenshotseuropa@gmail.com>
2019-01-07 19:09:16 +0000

    add ++

--- src/app/matching/matching.component.ts
+++ src/app/matching/matching.component.ts
@@ -89,10 +89,14 @@ export class MatchingComponent implements OnInit {
 
     const controls = this.data.workers.map(c => new FormControl(false));
     controls[0].setValue(true);
+
+    const controlsShift = this.data.workers.map(c => new FormControl(false));
+    controls[0].setValue(true);
     // console.log('controls', controls);
 
     this.form = this.formBuilder.group({
-      workers: new FormArray(controls, this.minSelectedCheckboxes(1))
+      workers: new FormArray(controls, this.minSelectedCheckboxes(1)),
+      shifts: new FormArray(controlsShift, this.minSelectedCheckboxes(1))
     });
     console.log('this.form', this.form);
   }
@@ -104,18 +108,34 @@ export class MatchingComponent implements OnInit {
       .map((v, i) => v ? this.data.workers[i].id : null)
       .filter(v => v !== null);
 
-    // console.log('selectedOrderIds->', selectedOrderIds);
+    const selectedOrderIdsShift = this.form.value.shifts
+      .map((v, i) => v ? this.data.shifts[i].id : null)
+      .filter(v => v !== null);
+
+    // console.log('selectedOrderIdsShift->', selectedOrderIdsShift);
+
     for (let i = 0; i < this.data.workers.length; i++) {
         // console.log('workers', this.data.workers[i]);
         for ( let j = 0; j < selectedOrderIds.length; j++ ) {
             // console.log('select', selectedOrderIds[j]);
             if (this.data.workers[i].id === selectedOrderIds[j]) {
               this.outData.workers.push(this.data.workers[i]);
-              console.log('DEBE HACER PUSH A LOS QUE SE ENVIA->', this.data.workers[i] );
+              // console.log('DEBE HACER PUSH A LOS QUE SE ENVIA->', this.data.workers[i] );
             }
         }
     }
-    console.log('this.outData.workers', this.outData);
+
+    for (let i = 0; i < this.data.shifts.length; i++) {
+        // console.log('shifts', this.data.shifts[i]);
+        for ( let j = 0; j < selectedOrderIdsShift.length; j++ ) {
+            // console.log('select', selectedOrderIds[j]);
+            if (this.data.shifts[i].id === selectedOrderIdsShift[j]) {
+              this.outData.shifts.push(this.data.shifts[i]);
+              // console.log('DEBE HACER PUSH A LOS QUE SE ENVIA->', this.data.shifts[i] );
+            }
+        }
+    }
+    // console.log('this.outData.workers', this.outData);
     this.sendDataPost(this.outData);
   }
 
@@ -127,13 +147,12 @@ export class MatchingComponent implements OnInit {
 
       return totalSelected >= min ? null : { required: true };
     };
-    console.log('validator--->', validator);
     return validator;
   }
 
   sendDataPost(data) {
     // this.selectedWorkers = this.matchingGroup.value;
-    console.log('--', this.selectedWorkers);
+  // console.log('--ENVIA ----->', data);
   return this.httpService
           .sendPost(data, this._URL)
           .subscribe(resp => {
@@ -141,42 +160,23 @@ export class MatchingComponent implements OnInit {
               this.matching = this._RESPONSE.res;
               this.msmOK = this._RESPONSE.msm;
               this.msmERR = this._RESPONSE.err;
-              console.log('VUELTA DEL SERVER- API:>', this.matching);
-              console.log('VUELTA DEL SERVER- API:>' , this._RESPONSE);
-              console.log('msmOK', this.msmOK);
+              // console.log('VUELTA DEL SERVER- API: <------', this.matching);
+              // console.log('VUELTA DEL SERVER- API: <------' , this._RESPONSE);
+              // console.log('msmOK', this.msmOK);
               this.openSnackBar(this.msmERR  , 'close');
               this.show = true;
+              this.cleanForm();
             }, error => { console.log(error);
           });
-    console.log(data);
+    // console.log(data);
   }
 
-  addWorkers(data) {
-    console.log('add work --->', data);
+  cleanForm() {
+    this.outData = { 'workers': [], 'shifts': [] };
   }
 
-  // sendPostTestOLD(testData: any) {
-
-  //   // const body = JSON.stringify(testData);
-  //   const body = testData;
-  //   console.log('send data--> ', body);
-  //   const headerOptions = new HttpHeaders({ 'Content-Type': 'application/json' });
-
-  //   return this.http.post(this._URL, body, {headers: headerOptions})
-  //     .subscribe( res => {
-
-  //       this._RESPONSE = res;
-  //       console.log('VUELTA DEL SERVER- API:>' , this._RESPONSE);
-  //       this.openSnackBar( this._RESPONSE.res , 'close');
-  //       this.show = true;
-
-  //     }, error => { console.log(error);
-  //     });
-
-  // }
 
   openSnackBar(message, action ) {
-
     this.snackBar.open(message, action, {
       duration: 3500,
       panelClass: 'success-dialog'
@@ -196,49 +196,8 @@ function minSelectedCheckboxes(min = 1) {
       .reduce((prev, next) => next ? prev + next : prev, 0);
 
     return totalSelected >= min ? null : { required: true };
-    console.log('validator--->', validator);
 
   };
   return validator;
 }
-/*
-  workersTest = [
-    {
-      'id': 1,
-      'availability': ['Monday', 'Wednesday'],
-      'payrate': 7.50
-    },
-    {
-      'id': 2,
-      'availability': ['Monday', 'Tuesday', 'Thursday'],
-      'payrate': 9.00
-    },
-    {
-      'id': 3,
-      'availability': ['Monday', 'Friday'],
-      'payrate': 18.00
-    },
-    {
-      'id': 4,
-      'availability': ['Monday', 'Tuesday', 'Friday'],
-      'payrate': 12.25
-    }
-  ];
-  shiftsTest = [
-    {
-      'id': 1,
-      'day': ['Monday']
-    },
-    {
-      'id': 2,
-      'day': ['Tuesday']
-    },
-    {
-      'id': 3,
-      'day': ['Wednesday']
-    },
-    {
-      'id': 4,
-      'day': ['Thursday']
-    }
-  ]; */
+
